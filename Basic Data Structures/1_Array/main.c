30ab428d9daca2b0dc99516dd8af3e473c559b26
Sami Haddad <77640501+samihaddad99@users.noreply.github.com>
2024-11-10 22:43:28 -0500

    implemented arrays

--- Basic Data Structures/1_Array/main.c
+++ Basic Data Structures/1_Array/main.c
@@ -1,15 +1,72 @@
 #include <stdio.h>
-#define NUMBER_ARR_SIZE 3
+#include <string.h>
+
+#define INT_UNALLOC -128 // manually set the size of unallocated values (for some reason windows is using 8bit signed integers)
+
+typedef struct {
+    int capacity; // total capacity of array
+    int size; // current # of element inside array
+    char* ptr; // pointer to array (block of contiguous memory)
+} intArray;
+
+void initArray(intArray *arr, int capacity){
+    arr->ptr = (int *)malloc(capacity * sizeof(int)); // create the block of contiguous memory
+    arr->capacity = capacity;
+    for(int i = 0; i < capacity; i++){ // initialize values to max int size
+        arr->ptr[i] = INT_UNALLOC;
+    }
+}
+
+void addElement(intArray *arr, int index, int value){
+    printf("adding element %d value %d\n", index, value);
+    if(arr->size >= arr->capacity){ // if arr full condition
+        printf("Array is full. Failed to add element.\n");
+        return;
+    }
+    if(index > arr->capacity){
+        printf("Index out of Bound. Failed to add element.\n");
+        return;
+    }
+    arr->ptr[index] = value; // add value to the first free block of memory
+    arr->size++; // increment the size (keeping track)
+}
+
+void removeElement(intArray *arr, int index){
+    printf("remove element %d \n", index);
+    if(index > arr->capacity){
+        printf("Index out of Bound. Failed to add element.\n");
+        return;
+    }
+    int max = INT_UNALLOC;
+    arr->ptr[index] = max;
+    arr->size--; // decrement the size
+}
+
+void printArray(intArray *arr){
+    for(int i = 0; i < arr->capacity; i++){
+        printf("Array index %d, value: %d\n", i, arr->ptr[i]);
+    }
+}
+// Clean up allocated memory
+void freeArray(intArray *arr) {
+    free(arr->ptr);
+}
+
 int main()
 {
-    char str[] = "hello there";
+    intArray array;
+    initArray(&array, 5);
 
-    int nums[4] = {4,5,6};
+    addElement(&array, 0, 10);
+    addElement(&array, 2, 5);
+    addElement(&array, 4, 2);
+    addElement(&array, 3, 1);
 
-    for(int i = 0; i < NUMBER_ARR_SIZE; i++){
-        printf("index: %d, value: %d\n", i, nums[i]);
-    }
-    
+    removeElement(&array, 3);
+
+    printArray(&array);
+
+    freeArray(&array);
 
     return(0);
 }
