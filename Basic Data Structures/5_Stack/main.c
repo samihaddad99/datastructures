1a2fa8748c5dd0b20fb2f5ad93d3824f944025d2
samihaddad85 <haddad0@sheridancollege.ca>
2024-11-12 13:09:54 -0500

    started stacks

---
+++ Basic Data Structures/5_Stack/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef struct  {
+    int value;
+    int size;
+    int capacity;
+} stack;
+
+/*
+* push - push to the first available space in the stack (size)
+* @param (stack) *stk - stack pointer reference
+* @param (int) value - value to add to the stack
+*/
+void push(stack *stk , int value){
+
+}
+
+/*
+* pop - pop the last element in the stack (size)
+* @param (stack) *stk - stack pointer reference
+* @return (int) - value of the last element in the stack
+*/
+int pop(stack *stk){
+
+}
+
+int main() {
+
+    return 0;
+}
